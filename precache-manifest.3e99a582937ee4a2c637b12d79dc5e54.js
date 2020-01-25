a6cf765709fadebda36f4d29383586202a693fe8
chanChanMan <chanChanMan@users.noreply.github.com>
2020-01-25 11:56:08 +0530

    second commit

---
+++ precache-manifest.3e99a582937ee4a2c637b12d79dc5e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e0ece3160159d6fd4e4d56c7bbfcf76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d786b0c3d9f19f7669ae",
+    "url": "/static/css/main.2bb1c270.chunk.css"
+  },
+  {
+    "revision": "6f20bf09af618e706953",
+    "url": "/static/js/2.c9d4a5a7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c9d4a5a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d786b0c3d9f19f7669ae",
+    "url": "/static/js/main.96eafd4c.chunk.js"
+  },
+  {
+    "revision": "a4fdd29660a9ca8ee13e",
+    "url": "/static/js/runtime-main.c80d8c58.js"
+  }
+]);
